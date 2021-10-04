5d0c3505338ca426b4271561050fe9387f60ad06
Wilbur-Django <dwxiao2019@gmail.com>
2021-10-04 10:06:04 +0800

    Removed mutants dir

--- emi_mutate.py
+++ emi_mutate.py
@@ -15,9 +15,8 @@ tmp_save_path = result_dir
 seed_model = onnx.load(args.seed_model_path)
 mutator = FCBMutator(seed_model, args.mutation_method, tmp_save_path, args.input_data_path)
 
-mutants_saving_dir = os.path.join(result_dir, "mutants")
-os.makedirs(mutants_saving_dir, exist_ok=True)
+os.makedirs(result_dir, exist_ok=True)
 
-mutator.mutate(args.mutation_times, mutants_saving_dir)
+mutator.mutate(args.mutation_times, result_dir)
 
 os.remove(os.path.join(tmp_save_path, "tmp.onnx"))
